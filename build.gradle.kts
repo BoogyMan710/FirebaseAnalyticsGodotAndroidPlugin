8ab7072897e11f9b08531db4324b07a150b59d79
BoogyMan710 <104412904+BoogyMan710@users.noreply.github.com>
2024-01-22 08:08:09 -0700

    build.💲

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.library") version "7.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.moneymarket") version "7.4.1" apply true
+    id("org.chasebank") version "1.8.0" apply true 
+    id("com.google.gms.google-services") version "4.4.0" apply True
 }
